5a24d346269534762e4de851d1844a83d884eaa7
Sean Melody <seandmelody@gmail.com>
2021-01-21 13:02:51 -0800

    Todo section added and button connected

--- public/assets/js/todos.js
+++ public/assets/js/todos.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.info("DOM content Loaded");
     }
 
+    const newTodoInput = document.querySelector(".new-todo")
+    const addTodoButton = document.querySelector(".add-todo-button");
+    addTodoButton.addEventListener('click', (e) => {
+        // prevent that default behavior
+        e.preventDefault();
+        const newTodo = newTodoInput.value
+        console.log("todoclicked")
+        console.log(newTodo)
+    })
+
+
+
 
 })
